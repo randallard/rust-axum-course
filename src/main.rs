b63e9a26f1cc80d66d60f3aebf8ace52061c3001
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-03-21 13:59:40 -0700

    first server / route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+#[allow(unused)]
+use axum::response::Html;
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+    // region: --- Start Server
+    let addr = SocketAddr::from(([127,0,0,1],8080));
+    println!("->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+    .serve(routes_hello.into_make_service())
+    .await
+    .unwrap()
 }
