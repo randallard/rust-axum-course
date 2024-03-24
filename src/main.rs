d8a9886d994109d8f231eaa997a28878508c6dc7
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-03-24 08:08:44 -0700

    add CRUD Implementation

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@
 pub use self::error::{Error, Result};
 
 use axum::extract::{Path, Query};
-use axum::response::Html;
-use axum::response::IntoResponse;
+use axum::response::{Html,IntoResponse,Response};
 use axum::routing::get;
 use axum::routing::get_service;
-use axum::Router;
+use axum::{middleware, Router};
 use serde::Deserialize;
+use tower_cookies::CookieManagerLayer;
 use std::net::SocketAddr;
 use tower_http::services::ServeDir;
 
 mod error;
+mod model;
 mod web;
 
 #[tokio::main]
@@ -20,6 +21,8 @@ async fn main() {
     let routes_all = Router::new()
     .merge(routes_hello())
     .merge(web::routes_login::routes())
+    .layer(middleware::map_response(main_response_mapper))
+    .layer(CookieManagerLayer::new())
     .fallback_service(routes_static());
 
     // region: --- Start Server
@@ -32,6 +35,13 @@ async fn main() {
     .unwrap()
 
     // endregion: --- Start Server
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    println!();
+    res
 }
 
 fn routes_static() -> Router {
